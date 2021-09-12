303ca604c3d1a8a74bf6d36e869273dc605b5529
Yuta <yuta.dammatsu@techouse.jp>
2021-09-12 21:06:20 +0900

    env 変更

--- config/initializers/grpc.rb
+++ config/initializers/grpc.rb
@@ -2,5 +2,5 @@ require 'pb/microservice_pb'
 require 'pb/microservice_services_pb'
 
 MICRO_SERVICE_STUB = MicroService::Pb::MicroService::Stub.new(
-                       'microservice:3001', :this_channel_is_insecure
+                       "#{ENV.fetch('MICRO_SERVICE_HOST', 'microservice')}:3001", :this_channel_is_insecure
                      )
